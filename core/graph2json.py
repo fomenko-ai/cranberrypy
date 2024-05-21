4388e93741d5bef8289690d8c0f35c2578b176d1
aleksei <fal@exante.eu>
2024-05-21 14:03:57 +0400

    CAS-836 Refactor graph2json.py to check path existence
    The existing code in graph2json.py has been refactored to first check if the 'path' attribute of a source module exists before checking if it ends with '.py'. This improved approach helps to avoid potential AttributeErrors when path is None, enhancing the robustness of the code.

--- core/graph2json.py
+++ core/graph2json.py
@@ -8,26 +8,28 @@ def graph2json(graph: DepGraph):
     for module_name, source in graph.sources.items():
         if module_name.endswith('.py'):
             continue
-        if not source.path.endswith('.py'):
-            continue
-        module = ImportModule(module_name, source.path)
-        if module.is_empty:
-            continue
-        if module.type in ['built_in', 'third_party']:
-            continue
-        for import_name in source.imports:
-            if import_name.endswith('.py'):
+        if source.path and source.path.endswith('.py'):
+            module = ImportModule(module_name, source.path)
+            if module.is_empty:
                 continue
-            if import_name in graph.sources:
-                if graph.sources[import_name].path.endswith('.py'):
-                    import_type = ImportModule.get_type(graph.sources[import_name].path)
-                    if import_type:
-                        module.imports[import_type].append(import_name)
-                    else:
-                        module.parse_class_imports(
-                            graph.sources[import_name].name
+            if module.type in ['built_in', 'third_party']:
+                continue
+            for import_name in source.imports:
+                if import_name.endswith('.py'):
+                    continue
+                if import_name in graph.sources:
+                    if graph.sources[import_name].path and\
+                            graph.sources[import_name].path.endswith('.py'):
+                        import_type = ImportModule.get_type(
+                            graph.sources[import_name].path
                         )
-        imports.append(module)
+                        if import_type:
+                            module.imports[import_type].append(import_name)
+                        else:
+                            module.parse_class_imports(
+                                graph.sources[import_name].name
+                            )
+            imports.append(module)
 
     return {
         "imports": {module.name: module.imports for module in imports}
