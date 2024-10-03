1bdccf2a2ee48237a95e74200a902955936e086e
aleksei <fal@exante.eu>
2024-10-03 16:20:49 +0400

    Add tests for project module interactions
    Introduced comprehensive tests to validate the configuration setup and interactions between various project modules. Ensured the correctness of data transformations and dependencies throughout the process chain.

---
+++ tests/test_main.py
@@ -0,0 +1,68 @@
+from unittest.mock import patch
+
+from core.project import Project
+from core.utils.func import deep_dict_compare, deep_list_compare
+from tests.example_data import IMPORTS, EXPORTS, DIAGRAMS, ASSISTANT
+from tests.example_data import get_config
+
+
+def test_main():
+    config = get_config()
+    with patch('core.logger.Logger'):
+        from core.modules.source_module import SourceModule
+        from core.converters.graph2imports import Graph2Imports
+        from core.converters.imports2exports import Imports2Exports
+        from core.converters.exports2diagrams import Exports2Diagrams
+        from core.converters.diagrams2assistant import Diagrams2Assistant
+
+        project = Project(config=config)
+        if project.file_paths:
+            graph2imports = Graph2Imports(config=config)
+            imports2exports = Imports2Exports(config=config)
+            exports2diagrams = Exports2Diagrams(config=config)
+            diagrams2assistant = Diagrams2Assistant(config=config)
+
+            from core.adapter.pydeps_lib import pydeps
+            graph = pydeps(config=config, workdir=config.project_path)
+
+            for file_path in project.file_paths:
+                try:
+                    module = SourceModule(file_path)
+                    if module.has_imports:
+                        graph2imports.add(module, graph)
+                except Exception as e:
+                    print(e)
+
+            assert graph2imports.data['modules']
+            assert deep_dict_compare(
+                IMPORTS['modules'],
+                graph2imports.data['modules'],
+                ['file_path']
+            )
+
+            imports2exports.add(graph2imports.data)
+            assert imports2exports.data['modules']
+            assert deep_dict_compare(
+                EXPORTS['modules'],
+                imports2exports.data['modules']
+            )
+
+            exports2diagrams.add(imports2exports.data)
+            assert exports2diagrams.data['nodes']
+            assert deep_list_compare(
+                DIAGRAMS['nodes'],
+                exports2diagrams.data['nodes']
+            )
+            assert deep_list_compare(
+                DIAGRAMS['links'],
+                exports2diagrams.data['links'],
+                ['fullInfo']
+            )
+
+            diagrams2assistant.add(graph2imports.data, exports2diagrams.data)
+            assert diagrams2assistant.data['dependencies']
+            assert deep_list_compare(
+                ASSISTANT['dependencies'],
+                diagrams2assistant.data['dependencies'],
+                ['source']
+            )
